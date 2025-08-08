4a3d897913ced10c2830bd3a845a2405b6276cdd
Soni Rusagara <soni.rusagara@gmail.com>
2025-08-07 20:30:13 -0400

    feat: add attachment options to action sheet with SF Symbols

--- NotesStream/Add Note Screen /AddNoteViewController.swift
+++ NotesStream/Add Note Screen /AddNoteViewController.swift
@@ -27,13 +27,47 @@ class AddNoteViewController: UIViewController {
         // Attach action methods
         screenView.saveButton.addTarget(self, action: #selector(saveTapped), for: .touchUpInside)
         screenView.shareButton.addTarget(self, action: #selector(shareTapped), for: .touchUpInside)
-        screenView.attachButton.addTarget(self, action: #selector(attachImageTapped), for: .touchUpInside)
+        screenView.attachButton.addTarget(self, action: #selector(attachTapped), for: .touchUpInside)
     }
     
-    @objc func attachImageTapped() {
-        // Show image picker logic here
-        print("Attach button tapped")
+    @objc func attachTapped() {
+        let picker = UIAlertController(title: "Add Attachment", message: nil, preferredStyle: .actionSheet)
+
+        // Photo
+        let photoAction = UIAlertAction(title: "Photo", style: .default, handler: { _ in
+            print("Attach photo tapped")
+        })
+        photoAction.setValue(UIImage(systemName: "photo"), forKey: "image")
+        picker.addAction(photoAction)
+
+        // File
+        let fileAction = UIAlertAction(title: "File", style: .default, handler: { _ in
+            print("Attach file tapped")
+        })
+        fileAction.setValue(UIImage(systemName: "doc"), forKey: "image")
+        picker.addAction(fileAction)
+
+        // Audio
+        let audioAction = UIAlertAction(title: "Audio", style: .default, handler: { _ in
+            print("Attach audio tapped")
+        })
+        audioAction.setValue(UIImage(systemName: "waveform"), forKey: "image")
+        picker.addAction(audioAction)
+
+        // Scan Document
+        let scanAction = UIAlertAction(title: "Scan Document", style: .default, handler: { _ in
+            print("Scan document tapped")
+        })
+        scanAction.setValue(UIImage(systemName: "doc.viewfinder"), forKey: "image")
+        picker.addAction(scanAction)
+
+        // Cancel
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        picker.addAction(cancel)
+
+        present(picker, animated: true)
     }
+
     
     @objc func saveTapped() {
         print("Save button tapped")
